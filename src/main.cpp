2489b36eccf1775aae04546424d10f30f8f217d7
melanieloredo <melanie.loredo01@utrgv.edu>
2025-01-23 22:47:22 -0600

    title screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include "bn_core.h"
+#include "bn_regular_bg_position_hbe_ptr.h"
+#include "bn_regular_bg_items_gba_screen.h"
+#include "bn_keypad.h"
+
+//display title screen
+void show_title()
+{
+    bn::regular_bg_ptr bg = bn::regular_bg_items::gba_screen.create_bg(0, 0);
+    bg.set_visible(true);
+
+    //wait for the user
+    while(true){
+        bn::core::update();
+
+        if(bn::keypad::a_pressed()){
+         break;
+        }
+    }
+}
 
 int main()
 {
     bn::core::init();
+
+    show_title();
 
     while(true)
     {
