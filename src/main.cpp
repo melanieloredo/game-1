26393840f3dcd011529b3289ec415280db940e00
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-01-25 12:23:33 -0600

    showing text

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,38 @@
 #include "bn_regular_bg_items_gba_screen.h"
 #include "bn_keypad.h"
 
-//display title screen
-void show_title()
-{
-    bn::regular_bg_ptr bg = bn::regular_bg_items::gba_screen.create_bg(0, 0);
-    bg.set_visible(true);
+#include "bn_sprite_ptr.h"
+#include "bn_sprite_text_generator.h"
+#include "fixed_32x64_sprite_font.h"
+#include "common_fixed_8x8_sprite_font.h"
 
-    //wait for the user
-    while(true){
-        bn::core::update();
+namespace {
+    //display title screen
+    void show_title()
+    {
+        bn::regular_bg_ptr bg = bn::regular_bg_items::gba_screen.create_bg(0, 0);
+        bg.set_visible(true);
 
-        if(bn::keypad::a_pressed()){
-         break;
+        //wait for the user
+        while(true){
+            bn::core::update();
+
+            if(bn::keypad::a_pressed()){
+            break;
+            }
+        }
+    }
+
+    void numberPicker() {
+        bn::sprite_text_generator text_generator(common::fixed_8x8_sprite_font);
+        // huge_text_generator.set_center_alignment();
+
+        bn::vector<bn::sprite_ptr, 32> text_sprites;
+        text_generator.generate(-120, -60, "Text", text_sprites);
+
+        while(! bn::keypad::start_pressed())
+        {
+            bn::core::update();
         }
     }
 }
@@ -24,6 +44,8 @@ int main()
     bn::core::init();
 
     show_title();
+
+    numberPicker();
 
     while(true)
     {
