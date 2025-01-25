f96f3facf8d2e48bbb27fdcbad2fa770fb1c14f2
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-01-25 15:07:30 -0600

    number selection screen

--- src/main.cpp
+++ src/main.cpp
@@ -10,42 +10,43 @@
 
 namespace {
     //display title screen
-    void show_title()
-    {
+    void show_title() {
         bn::regular_bg_ptr bg = bn::regular_bg_items::gba_screen.create_bg(0, 0);
         bg.set_visible(true);
 
         //wait for the user
-        while(true){
+        while(! bn::keypad::a_pressed()){
             bn::core::update();
-
-            if(bn::keypad::a_pressed()){
-            break;
-            }
         }
     }
 
-    void numberPicker() {
-        bn::sprite_text_generator text_generator(common::fixed_8x8_sprite_font);
-        // huge_text_generator.set_center_alignment();
+    void numberPicker(int value) {
+        bn::sprite_text_generator text_generator(fixed_32x64_sprite_font);
+        // bn::sprite_text_generator text_generator(common::fixed_8x8_sprite_font);
+        text_generator.set_center_alignment();
 
-        bn::vector<bn::sprite_ptr, 32> text_sprites;
-        text_generator.generate(-120, -60, "Text", text_sprites);
+        bn::string_view text[10] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
-        while(! bn::keypad::start_pressed())
-        {
+        while(! bn::keypad::start_pressed()) {
+            bn::vector<bn::sprite_ptr, 2> text_sprites;
+            if (bn::keypad::up_pressed() && value < 9) {
+                value += 1;
+            }
+            if (bn::keypad::down_pressed() && value > 0) {
+                value -= 1;
+            }
+            text_generator.generate(0, 0, text[value], text_sprites);
             bn::core::update();
         }
     }
 }
 
-int main()
-{
+int main() {
     bn::core::init();
 
     show_title();
 
-    numberPicker();
+    numberPicker(4);
 
     while(true)
     {
